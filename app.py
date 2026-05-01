30b780956db384df3bfbba904b637e5f2f379135
Neko-Kuroi <88179359+Neko-Kuroi@users.noreply.github.com>
2026-05-02 01:41:02 +0900

    Add static file mounting to FastAPI app
    Mounted static files to serve from the '/static' directory.

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
+# 静的ファイルをマウントするために
+from fastapi.staticfiles import StaticFiles
+
 from init_db import init_db
 import index
 import webhook
@@ -8,14 +11,15 @@ import thread
 import post
 import search
 
-
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     init_db()
     yield
 
-
 app = FastAPI(lifespan=lifespan)
+
+# 静的ファイルをマウント
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(index.router)
 app.include_router(webhook.router)
